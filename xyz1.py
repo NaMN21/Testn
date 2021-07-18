43818d508ed530cdccca3c25dcdef12501cc6dcc
NaMN21 <87276322+NaMN21@users.noreply.github.com>
2021-07-18 13:22:56 +0200

    Hello Git

---
+++ xyz1.py
@@ -0,0 +1,19 @@
+# #print(1+1+1)
+# #isinstance("null",str)
+
+# x=(9,2,3)
+# #print(x)
+# y=[3,6,9,5,23,32,333,233,355,[2,9,4,1],3,5,9,9,76]
+# #print(y)
+# #print(y[-12:-1])
+# #print(y[7:11])
+# # print(y[2:])
+# # print(y[:8])
+# # print(y.count)
+# # fruits = ['apple', 'banana', 'cherry']
+
+# # cars = ['Ford', 'BMW', 'Volvo']
+
+# # fruits.extend(cars)
+# # print(fruits)
+print(" Cannot Communicate")
